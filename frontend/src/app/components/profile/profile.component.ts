befda2b73fe1a81c8fa797ba7402b3fd3a1ee9ca
Marc Sutthaloung <marc.sutthaloung@gmx.net>
2025-05-27 10:59:48 +0200

    wip

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { AuthService } from '../../services/auth/auth.service';
+import { RecordModel } from 'pocketbase';
+import { Router } from '@angular/router';
+import { redirectTo } from '../../utils/router-functions';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [DatePipe, HeaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  public user: RecordModel;
+  private emptyUser: RecordModel = {
+    id: '',
+    collectionId: '',
+    collectionName: '',
+  };
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {
+    const loggedInUser = this.authService.getLoggedInUser();
+    this.user = loggedInUser ?? this.emptyUser;
+    if (!loggedInUser) redirectTo('', this.router);
+  }
+}
