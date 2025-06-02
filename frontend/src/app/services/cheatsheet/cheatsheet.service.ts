fca00864180a87e24f374ec09e854efe0c8969af
Raphaël Schreiber <raphael_mathieu.schreiber@sbb.ch>
2025-06-02 07:27:01 +0200

    working on content of base site (#8)
    * working on content of base site
    
    * finished the content of the site
    
    * run npm prettier fix
    
    * also add stars possibility and header improvements
    
    * again run npm prettier fix

--- frontend/src/app/services/cheatsheet/cheatsheet.service.ts
+++ frontend/src/app/services/cheatsheet/cheatsheet.service.ts
@@ -7,6 +7,7 @@ import PocketBase, { RecordModel } from 'pocketbase';
 })
 export class CheatsheetService {
   private pb = new PocketBase('http://localhost:8090');
+
   constructor() {}
 
   public async create(cheatsheet: Cheatsheet): Promise<RecordModel | null> {
@@ -15,7 +16,16 @@ export class CheatsheetService {
     }
     throw new Error('invalid cheatsheet');
   }
+
   public getAllCheatsheets(): Promise<RecordModel[]> {
     return this.pb.collection('cheatsheets').getFullList();
+  }
+
+  public async getTopFourCheatSheets(): Promise<RecordModel[]> {
+    const fullList = await this.getAllCheatsheets();
+
+    return fullList
+      .sort((a, b) => (b['stars'] ?? 0) - (a['stars'] ?? 0))
+      .slice(0, 4);
   }
 }
