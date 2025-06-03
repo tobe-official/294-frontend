3601344cba8576be4c69c64a20c91a7f95961610
Daniel Butler <github@danielbutler.ch>
2025-06-03 13:34:03 +0200

    fix migration

---
+++ backend/migrations/1748950362_updated_cheatsheets.go
@@ -0,0 +1,43 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1722640318")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "number1092079998",
+			"max": null,
+			"min": 1,
+			"name": "credits",
+			"onlyInt": true,
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1722640318")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("number1092079998")
+
+		return app.Save(collection)
+	})
+}
